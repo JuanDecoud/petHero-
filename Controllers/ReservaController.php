2dc17f4c44c15004d6631d89d45cf7abccf5f9ae
JuanDecoud <90573318+JuanDecoud@users.noreply.github.com>
2022-10-25 07:21:18 -0300

    Update ReservaController.php

--- Controllers/ReservaController.php
+++ Controllers/ReservaController.php
@@ -1,7 +1,21 @@
 <?php 
     namespace Controllers ;
 
+use DAO\KeeperDAO;
+use DAO\PetDAO ;
+
     class ReservaController {
+
+        private KeeperDAO $keeperdao;
+        private PetDAO $petdao ;
+
+        public function __construct()
+        {
+            $this->keeperdao = new KeeperDAO();
+            $this->petdao = new PetDAO ();
+            
+        }
+
 
         public function prueba ($nombre , $fecha , $fecha2){
             echo $nombre.$fecha.$fecha2;
@@ -10,6 +24,12 @@
 
         public function vistaOwner (){
             require_once (VIEWS_PATH."menu-owner.php");
+        }
+
+
+        public function solicitadudEstadia ($nombreKeeper , $fechainicio , $fechaFin){
+                $user = $this->keeperdao->obtenerUser($nombreKeeper);
+                
         }
     }
 ?>
